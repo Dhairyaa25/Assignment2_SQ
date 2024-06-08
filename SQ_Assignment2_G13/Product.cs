f2cca44b3caef6fa8b765871b6123f5324778eb9
Dhairyaa25 <dhairyabhavsar01@gmail.com>
2024-06-07 21:22:04 -0400

    first commit

---
+++ SQ_Assignment2_G13/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SQ_Assignment2_G13
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int productId, string productName, decimal price, int stock)
+        {
+            ProductID = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            if (amount > 0)
+            {
+                Stock += amount;
+            }
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            if (amount > 0 && amount <= Stock)
+            {
+                Stock -= amount;
+            }
+        }
+    }
+
+}
---
+++ SQ_Assignment2_G13_Test/ProductTest.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+using SQ_Assignment2_G13;
+
+namespace SQ_Assignment2_G13.Tests
+{
+    [TestFixture]
+    public class ProductTests
+    {
+       
+    }
+}
